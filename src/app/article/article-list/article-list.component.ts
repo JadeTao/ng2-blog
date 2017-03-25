cafbaa05c1c53444c57d008248d88da57c990f68
jader_tao <Jader_tao@qq.com>
2017-03-26 04:28:20 +0800

    flex &some new layout

--- src/app/article/article-list/article-list.component.ts
+++ src/app/article/article-list/article-list.component.ts
@@ -23,7 +23,7 @@ export class ArticleListComponent implements OnInit {
     ngOnInit() {
        this.list=this.listService.getList();
        for( let n of this.list ){
-           n.preview = n.content.replace(/\#+/g,"").replace(/\>+/g,"").slice(0,200)         //正则提取汉字，提取预览
+           n.preview = n.content.replace(/\#+/g,"").replace(/\>+/g,"").replace(/\((http|ftp|https):\/\/[\w\-_]+(\.[\w\-_]+)+([\w\-\.,@?^=%&amp;:/~\+#]*[\w\-\@?^=%&amp;/~\+#])?\)/g,'').slice(0,200)         //正则提取汉字，提取预览
        }
     }
 }
---
+++ src/app/article/article-menu/article-menu.component.ts
@@ -0,0 +1,22 @@
+import {Component, OnInit} from '@angular/core'
+
+
+@Component({
+    selector: 'app-article-menu',
+    templateUrl: './article-menu.component.html',
+    styleUrls: ['./article-menu.component.scss'],
+    providers: []
+})
+export class ArticleMenuComponent implements OnInit {
+
+    constructor(
+       
+    ) {}
+
+  
+     
+
+    ngOnInit() {
+      
+    }
+}
