86e14c011a589e9a9e6c6af1b6cffd44fab4866a
jader_tao <Jader_tao@qq.com>
2017-04-25 23:22:54 +0800

    static

---
+++ src/app/static/mock-img.ts
@@ -0,0 +1,142 @@
+import { Img } from './entitis'
+
+export let IMG: Img[] = [{
+    id: 1,
+    src: "http://om1o3l1z1.bkt.clouddn.com/1.jpeg",
+    title: "晏几道",
+    caption: "晏几道",
+    desc: `
+    今日政事堂中半吾家旧客，
+    亦未暇见也。`
+}, {
+    id: 2,
+    src: "http://om1o3l1z1.bkt.clouddn.com/2.jpeg",
+    title: "黄景仁",
+    caption: "黄景仁",
+    desc: `
+    十有九人堪白眼，
+    百无一用是书生。`
+}, {
+    id: 3,
+    src: "http://om1o3l1z1.bkt.clouddn.com/3.jpeg",
+    title: "青玉案",
+    caption: "青玉案",
+    desc: `
+    凤箫声动，
+    玉壶光转，
+    一夜鱼龙舞`
+}, {
+    id: 4,
+    src: "http://om1o3l1z1.bkt.clouddn.com/4.jpeg",
+    title: "酌酒与裴迪",
+    caption: "酌酒与裴迪",
+    desc: `
+    白首相知犹按剑，
+    朱门先达笑弹冠。`
+}, {
+    id: 5,
+    src: "http://om1o3l1z1.bkt.clouddn.com/5.jpeg",
+    title: "热风",
+    caption: "热风",
+    desc: `愿中国青年都摆脱冷气，只是向上走，不必听自暴自弃者流的话。能做事的做事，能发声的发声。有一分热，发一分光，就令萤火一般，也可以在黑暗里发一点光，不必等候炬火。此后如竟没有炬火，我便是唯一的光。`
+}, {
+    id: 6,
+    src: "http://om1o3l1z1.bkt.clouddn.com/6.jpeg",
+    title: "下雪啦",
+    caption: "下雪啦",
+    desc: `
+    下雪啦，下雪啦！
+    雪地里来了一群小画家。
+    小鸡画竹叶，小狗画梅花，小鸭画枫叶，小马画月牙。
+    不用颜料不用笔，几步就成一幅画。`
+}, {
+    id: 7,
+    src: "http://om1o3l1z1.bkt.clouddn.com/7.jpeg",
+    title: "桃花扇",
+    caption: "桃花扇",
+    desc: `
+    眼见他起高楼，
+    眼见他宴宾客，
+    眼见他楼塌了。`
+}, {
+    id: 8,
+    src: "http://om1o3l1z1.bkt.clouddn.com/8.jpeg",
+    title: "沈德潜",
+    caption: "沈德潜",
+    desc: `
+    不怨霜露，
+    而怨春风；
+    见盛之始，
+    已伏衰之机也。`
+}, {
+    id: 9,
+    src: "http://om1o3l1z1.bkt.clouddn.com/9.jpeg",
+    title: "在本布尔山下",
+    caption: "在本布尔山下",
+    desc: `
+    cast a cold eye,
+    on life, on death,
+    horseman, pass by.`
+}, {
+    id: 10,
+    src: "http://om1o3l1z1.bkt.clouddn.com/10.jpeg",
+    title: "黄金时代",
+    caption: "黄金时代",
+    desc: `
+    那一天我二十一岁，
+    在我一生的黄金时代。
+    我有好多奢望。
+    我想爱，想吃，
+    还想在一瞬间变成天上半明半暗的云。`
+}, {
+    id: 11,
+    src: "http://om1o3l1z1.bkt.clouddn.com/11.jpeg",
+    title: "望江南 江南柳",
+    caption: "望江南 江南柳",
+    desc: `
+    恁时相见早留心,
+    何况到如今`
+}, {
+    id: 12,
+    src: "http://om1o3l1z1.bkt.clouddn.com/12.jpeg",
+    title: "襄阳歌",
+    caption: "襄阳歌",
+    desc: `
+    泪亦不能为之堕，心亦不能为之哀。
+    清风朗月不用一钱买，玉山自倒非人推。`
+}, {
+    id: 13,
+    src: "http://om1o3l1z1.bkt.clouddn.com/13.jpeg",
+    title: "麦田里的守望者",
+    caption: "麦田里的守望者",
+    desc: `
+    说来好笑。
+    你千万别跟任何人谈任何事情。
+    你只要一谈起，
+    就会想念起每一个人。`
+}, {
+    id: 14,
+    src: "http://om1o3l1z1.bkt.clouddn.com/14.jpeg",
+    title: "射雕英雄传",
+    caption: "射雕英雄传",
+    desc: `
+    四张机，
+    鸳鸯织就欲双飞。
+    可怜未老先白头，
+    春波碧草，
+    晓寒深处，
+    相对浴红衣。`
+}, {
+    id: 15,
+    src: "http://om1o3l1z1.bkt.clouddn.com/15.jpeg",
+    title: "红楼梦",
+    caption: "红楼梦",
+    desc: `
+    你是个可人,
+    你是个多情，
+    你是个刁钻古怪鬼精灵，
+    你是个神仙也不灵，
+    我说的话儿你全不信，
+    只叫你去背地里细打听，
+    才知道我疼你不疼!`
+}]
